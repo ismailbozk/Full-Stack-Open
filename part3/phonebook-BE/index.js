1a28b77f64b12aab2c8ba911b009f7f21df0fd0f
Ismail Bozkurt <ismailbozk@gmail.com>
2025-10-24 17:27:34 +0100

    part 3 3.12 exercise completed

--- part3/phonebook-BE/index.js
+++ part3/phonebook-BE/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const crypto = require("crypto");
-
-const app = express();
+const cors = require('cors')
 const morgan = require("morgan");
 
-const cors = require('cors')
+const app = express();
 app.use(cors())
-
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 function morganLogger(request, response, next) {
   morgan.token("body", function (req, res) {
