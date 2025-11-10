e886c448a103d6cee32bba2dcf2c87a7b1785457
Ismail Bozkurt <ismailbozk@gmail.com>
2025-11-10 16:57:06 +0000

    added eslint plug in and fixed all error/warnings

--- part3/phonebook-BE/index.js
+++ part3/phonebook-BE/index.js
@@ -1,31 +1,30 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const crypto = require("crypto");
-const cors = require("cors");
-const morgan = require("morgan");
-const Phonebook = require("./models/phonebook");
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const Phonebook = require('./models/phonebook')
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static("dist"));
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 
 function morganLogger(request, response, next) {
-  morgan.token("body", function (req, res) {
-    return JSON.stringify(req.body);
-  });
+  morgan.token('body', function (req) {
+    return JSON.stringify(req.body)
+  })
   const logger = morgan(
-    ":method :url :status :res[content-length] - :response-time ms :body"
-  );
-  logger(request, response, next);
+    ':method :url :status :res[content-length] - :response-time ms :body'
+  )
+  logger(request, response, next)
 }
-app.use(morganLogger);
+app.use(morganLogger)
 
-app.get("/info", async (request, response, next) => {
+app.get('/info', async (request, response, next) => {
   try {
     // This line gets the total count of documents in the Phonebook collection
-    const count = await Phonebook.countDocuments({});
+    const count = await Phonebook.countDocuments({})
 
     response.send(`<!doctype html>
 <html>
@@ -38,78 +37,78 @@ app.get("/info", async (request, response, next) => {
     <p>Phonebook has info for ${count} people</p>
     <p>${new Date()}</p>
   </body>
-</html>`);
+</html>`)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.get("/api/persons", async (request, response, next) => {
+app.get('/api/persons', async (request, response, next) => {
   try {
-    const persons = await Phonebook.find({});
-    response.json(persons);
+    const persons = await Phonebook.find({})
+    response.json(persons)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
   Phonebook.findById(id)
     .then((found) => {
       if (found) {
-        response.json(found);
+        response.json(found)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", async (request, response, next) => {
+app.delete('/api/persons/:id', async (request, response, next) => {
   try {
-    const id = request.params.id;
-    const result = await Phonebook.findByIdAndDelete(id);
+    const id = request.params.id
+    const result = await Phonebook.findByIdAndDelete(id)
 
     if (!result) {
-      return response.status(404).json({ error: "Person not found" });
+      return response.status(404).json({ error: 'Person not found' })
     }
 
-    response.status(204).end();
+    response.status(204).end()
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.post("/api/persons", async (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', async (request, response, next) => {
+  const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name or number is missing",
-    });
+      error: 'name or number is missing',
+    })
   }
 
   const person = Phonebook({
     name: body.name,
     number: body.number,
-  });
+  })
 
   try {
-    await person.save();
-    response.json(person);
+    await person.save()
+    response.json(person)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.put("/api/persons/:id", async (request, response, next) => {
-  console.log("PUT /api/persons called");
-  const id = request.params.id;
-  const body = request.body;
+app.put('/api/persons/:id', async (request, response, next) => {
+  console.log('PUT /api/persons called')
+  const id = request.params.id
+  const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name or number is missing",
-    });
+      error: 'name or number is missing',
+    })
   }
 
   try {
@@ -117,40 +116,40 @@ app.put("/api/persons/:id", async (request, response, next) => {
       id,
       { name: body.name, number: body.number },
       { new: true, runValidators: true }
-    );
+    )
 
     if (!updatedPerson) {
-      return response.status(404).json({ error: "Person not found" });
+      return response.status(404).json({ error: 'Person not found' })
     }
 
-    response.json(updatedPerson);
+    response.json(updatedPerson)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // handler of requests with unknown endpoint
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook-BE/mongo.js
+++ part3/phonebook-BE/mongo.js
@@ -1,61 +1,58 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
-}
+const password = process.argv[2]
+const username = process.argv[3]
+const phoneNumber = process.argv[4]
 
-const password = process.argv[2]; // 
-const username = process.argv[3];
-const phoneNumber = process.argv[4];
+const uri = `mongodb+srv://ismailbozk_db_user:${password}@cluster0.bj935i6.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=Cluster0`
 
-const uri = `mongodb+srv://ismailbozk_db_user:${password}@cluster0.bj935i6.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
 function createPhonebookSchemModel() {
   const phonebookSchema = new mongoose.Schema({
     name: String,
     number: String,
-  });
+  })
 
-  const PhonebookModel = mongoose.model("Phonebook", phonebookSchema);
-  return PhonebookModel;
+  const PhonebookModel = mongoose.model('Phonebook', phonebookSchema)
+  return PhonebookModel
 }
 
 function listOfPhonebookEntries() {
-  const PhonebookModel = createPhonebookSchemModel();
-    PhonebookModel.find({}).then((result) => {  
-    console.log("phonebook:");
+  const PhonebookModel = createPhonebookSchemModel()
+  PhonebookModel.find({}).then((result) => {
+    console.log('phonebook:')
     result.forEach((entry) => {
-      console.log(`${entry.name} ${entry.number}`);
-    });
-    console.log("----");
-    mongoose.connection.close();
-  });
+      console.log(`${entry.name} ${entry.number}`)
+    })
+    console.log('----')
+    mongoose.connection.close()
+  })
 }
 
-function createPhonebookEntry(name, number) {
-  const PhonebookModel = createPhonebookSchemModel();
+function createPhonebookEntry() {
+  const PhonebookModel = createPhonebookSchemModel()
 
   const phonebookEntry = new PhonebookModel({
     name: username,
     number: phoneNumber,
-  });
+  })
 
   phonebookEntry.save().then(() => {
-    console.log(`added ${username} number ${phoneNumber} to phonebook`);
-    mongoose.connection.close();
-  });
+    console.log(`added ${username} number ${phoneNumber} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
-mongoose.connect(uri);
+mongoose.connect(uri)
 
 if (process.argv.length === 3) {
-  listOfPhonebookEntries();
+  listOfPhonebookEntries()
 } else if (process.argv.length === 5) {
-  createPhonebookEntry(username, phoneNumber);
+  createPhonebookEntry(username, phoneNumber)
 } else {
   console.log(
-    "unexpected command format. Please use one of the formats below\n node mongo.js <password> <username> <phone number> \n node mongo.js <password>"
-  );
-  process.exit(1);
+    'unexpected command format. Please use one of the formats below\n node mongo.js <password> <username> <phone number> \n node mongo.js <password>'
+  )
+  process.exit(1)
 }
