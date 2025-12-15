57541d621e8128ddc4930fe7caaba45fbb232293
Ismail Bozkurt <ismail.bozkurt@zalando.ie>
2025-12-15 18:01:01 +0000

    exercise 10.5 part 1

--- part10/rate-repository-app/src/components/RepositoryItem.tsx
+++ part10/rate-repository-app/src/components/RepositoryItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
 import { Repository } from "../types/Repository";
 
 interface RepositoryItemProps {
@@ -13,12 +13,31 @@ function RepositoryItem({ repository }: RepositoryItemProps): React.ReactElement
             flexDirection: 'row',
             gap: 10,
         },
+        topTextContainer: {
+            flexGrow: 1,
+            flexShrink: 1,
+            flexDirection: 'column',
+            backgroundColor: '#b24d4dff',
+        },
+        text: {
+            flexShrink: 1,
+            paddingBottom: 10,
+        },
     });
 
     return (
         <View>
-            <Text>{repository.fullName}</Text>
-            <Text>{repository.description}</Text>
+            <View style={{ flexGrow: 1, flexDirection: 'row', margin: 10 }}>
+                <Image
+                    source={{ uri: repository.ownerAvatarUrl }}
+                    style={{ width: 50, height: 50, borderRadius: 5, marginRight: 10 }}
+                />
+                <View style={styles.topTextContainer}>
+                    <Text style={styles.text} numberOfLines={3} ellipsizeMode="tail">{repository.fullName}</Text>
+                    <Text style={styles.text} numberOfLines={3} ellipsizeMode="tail">{repository.description}</Text>
+                </View>
+            </View>
+
             <Text>{repository.language}</Text>
             <View style={styles.row}>
                 <Text>Forks: {repository.forksCount}</Text>
