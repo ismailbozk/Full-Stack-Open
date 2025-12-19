eb39dcb18c8a8ab954d36be347ee3db5c5b7a0e7
Ismail Bozkurt <ismail.bozkurt@zalando.ie>
2025-12-19 17:17:53 +0000

    exercise 10.12 part 2

--- part10/rate-repository-app/src/utils/apolloClient.ts
+++ part10/rate-repository-app/src/utils/apolloClient.ts
@@ -2,12 +2,12 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import Constants from 'expo-constants';
 
 const createApolloClient = () => {
-  if (!Constants.expoConfig?.extra?.apolloUri) {
+  if (!Constants.expoConfig?.extra?.apolloUri || typeof Constants.expoConfig?.extra?.apolloUri !== 'string') {
     throw new Error('APOLLO_URI is not defined in environment variables');
   }
 
   return new ApolloClient({
-    uri: Constants.expoConfig.extra.apolloUri,
+    uri: Constants.expoConfig?.extra?.apolloUri as string,
     cache: new InMemoryCache(),
   });
 };
