71ae5ea7d28a403b20cef756abaee5ba8945b78b
Ismail Bozkurt <ismail.bozkurt@zalando.ie>
2025-12-27 14:28:31 +0000

    exercise 10.18 missing test file

---
+++ part10/rate-repository-app/src/__tests__/components/RepositoryList.test.tsx
@@ -0,0 +1,80 @@
+import { expect, describe, it } from '@jest/globals';
+import { render, screen, within } from '@testing-library/react-native';
+
+import { RepositoryListContainer } from "../../components/Repository/RepositoryList";
+import { RepositoryListResponse, Repository } from '../../types/Repository';
+
+describe('RepositoryList', () => {
+    const repositoriesData: RepositoryListResponse = {
+        repositories: {
+            totalCount: 8,
+            pageInfo: {
+                hasNextPage: true,
+                hasPreviousPage: false,
+                endCursor: 'WyJhc3luYy1saWJyYXJ5LnJlYWN0LWFzeW5jIiwxNTg4NjU2NzUwMDc2XQ==',
+                startCursor: 'WyJqYXJlZHBhbG1lci5mb3JtaWsiLDE1ODg2NjAzNTAwNzZd',
+            },
+            edges: [
+                {
+                    node: {
+                        id: 'jaredpalmer.formik',
+                        fullName: 'jaredpalmer/formik',
+                        description: 'Build forms in React, without the tears',
+                        language: 'TypeScript',
+                        forksCount: 1619,
+                        stargazersCount: 21856,
+                        ratingAverage: 88,
+                        reviewCount: 3,
+                        ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
+                    },
+                    cursor: 'WyJqYXJlZHBhbG1lci5mb3JtaWsiLDE1ODg2NjAzNTAwNzZd',
+                },
+                {
+                    node: {
+                        id: 'async-library.react-async',
+                        fullName: 'async-library/react-async',
+                        description: 'Flexible promise-based React data loader',
+                        language: 'JavaScript',
+                        forksCount: 69,
+                        stargazersCount: 1760,
+                        ratingAverage: 72,
+                        reviewCount: 3,
+                        ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/54310907?v=4',
+                    },
+                    cursor: 'WyJhc3luYy1saWJyYXJ5LnJlYWN0LWFzeW5jIiwxNTg4NjU2NzUwMDc2XQ==',
+                },
+            ],
+        },
+    };
+
+    describe('RepositoryListContainer', () => {
+        it('renders repository information correctly', () => {
+
+            // Transform to Repository array
+            const repositoryArray: Repository[] = repositoriesData.repositories.edges.map(edge => edge.node);
+            render(<RepositoryListContainer repositories={repositoryArray} />);
+            const items = screen.getAllByTestId('RepositoryItem');
+
+            expect(items).toHaveLength(2);
+
+            const firstItem = within(items[0]);
+            const secondItem = within(items[1]);
+
+            expect(firstItem.getByText('jaredpalmer/formik')).toBeTruthy();
+            expect(firstItem.getByText('Build forms in React, without the tears')).toBeTruthy();
+            expect(firstItem.getByText('TypeScript')).toBeTruthy();
+            expect(firstItem.getByText('21.9k')).toBeTruthy();
+            expect(firstItem.getByText('1.6k')).toBeTruthy();
+            expect(firstItem.getByText('88')).toBeTruthy();
+            expect(firstItem.getByText('3')).toBeTruthy();
+
+            expect(secondItem.getByText('async-library/react-async')).toBeTruthy();
+            expect(secondItem.getByText('Flexible promise-based React data loader')).toBeTruthy();
+            expect(secondItem.getByText('JavaScript')).toBeTruthy();
+            expect(secondItem.getByText('1.8k')).toBeTruthy();
+            expect(secondItem.getByText('69')).toBeTruthy();
+            expect(secondItem.getByText('72')).toBeTruthy();
+            expect(secondItem.getByText('3')).toBeTruthy();
+        });
+    });
+});
